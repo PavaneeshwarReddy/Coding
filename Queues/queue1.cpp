4b6241060b046b630707ea664e1e062a088160dc
PavaneeshwarReddy <pavaneeshwar7077@gmail.com>
2022-11-29 22:10:45 +0530

    29-11-2022

--- Queues/queue1.cpp
+++ Queues/queue1.cpp
@@ -1,7 +1,71 @@
 //circular queue implementation
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-	
-}
+
+class MyCircularQueue {
+public:
+int front,rear;
+vector<int>cqueue;
+int n;
+    MyCircularQueue(int k) {
+            front=-1;
+            rear=-1;
+            n=k;
+        cqueue.resize(k);
+    }
+    
+    bool enQueue(int value) {
+        
+        if(isFull()==false)
+        {
+            rear=(rear+1)%n;
+            if(front==-1)
+                front=0;
+            cqueue[rear]=value;
+            return true;
+        }
+        return false;
+    }
+    
+    bool deQueue() {
+        if(isEmpty()==false)
+        {
+            if(rear==front)
+            {
+                rear=-1;
+                front=-1;
+            }
+            else
+            {
+                front=(front+1)%n;
+            }
+            return true;
+        }
+        return false;
+    }
+    
+    int Front() {
+       if(isEmpty())
+           return -1;
+         return cqueue[front];
+    }
+    
+    int Rear() {
+        if(isEmpty())
+           return -1;
+         return cqueue[rear];
+    }
+    
+    bool isEmpty() {
+        if(front==-1 && rear==-1)
+        return true;
+        return false;
+    }
+    
+    bool isFull() {
+        if(((rear+1)%n)==front)
+        return true;
+        return false;
+    }
+};
+
--- Queues/queue3.cpp
+++ Queues/queue3.cpp
@@ -1,13 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-	int q;
-	cin>>q;
-	while(q--)
-	{
-		int n;
-		cin>>n;
-		
-	}
-}
+
+
+
+/*
+The school cafeteria offers circular and square sandwiches at lunch break, referred to by numbers 0 and 1 respectively. All students stand in a queue. Each student either prefers square or circular sandwiches.
+
+The number of sandwiches in the cafeteria is equal to the number of students. The sandwiches are placed in a stack. At each step:
+
+If the student at the front of the queue prefers the sandwich on the top of the stack, they will take it and leave the queue.
+Otherwise, they will leave it and go to the queue's end.
+*/
+
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        stack<int>sand;
+		queue<int>stud;
+		for(int i=0;i<students.size();i++)
+		stud.push(students[i]);
+		for(int i=0;i<sandwiches.size();i++)
+		sand.push(sandwiches[i]);
+
+		int counter=0;
+		while(sand.size()!=0 && stud.size()!=0)
+		{
+			if(sand.top()==stud.front())
+			{
+				sand.pop();
+				stud.pop();
+				counter=0;
+			}
+			else
+			{
+				int temp=stud.front();
+				stud.pop();
+				stud.push(temp);
+				counter++;
+				if(counter==stud.size())
+				break;
+			}
+		}
+
+		return stud.size();
+
+    }
+};
